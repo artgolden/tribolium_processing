5cca98afd824bf7f09836fb6490d27ff55d6b981
artgolden <artemiy.golden@protonmail.com>
2022-03-01 11:48:50 +0100

    Proper angle and dims determination.

--- create_manual_crop_box.py
+++ create_manual_crop_box.py
@@ -41,36 +41,46 @@ from ij.gui import PointRoi, RotatedRectRoi
 def get_rotated_rect_roi_width():
 	img = WindowManager.getCurrentImage()
 	roi = img.getRoi()
-	print roi.getClass()
+	# print roi.getClass()
 	x1 = roi.getPolygon().xpoints[0]
 	x2 = roi.getPolygon().xpoints[1]
 	y1 = roi.getPolygon().ypoints[0]
 	y2 = roi.getPolygon().ypoints[1]
 	width = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 	width = (width / 10 ) * 10 
-	print(x1, y1, x2, y2)
-	print width
+	# print(x1, y1, x2, y2)
+	# print width
 
 
 def get_polygon_roi_angle(roi):
-	"""Returns an angle between first line in the PolygonRoi and horizontal line
+	"""Returns an angle between longer line in the rectangular PolygonRoi and horizontal line
 
 	Args:
 		roi (PolygonRoi): intended for rectangular Rois
 
 	Returns:
-		double: angle between first line in the PolygonRoi and horizontal line
+		double: angle between longer line in the PolygonRoi and horizontal line
 	"""
 	x1 = roi.getPolygon().xpoints[0]
 	x2 = roi.getPolygon().xpoints[1]
+	x3 = roi.getPolygon().xpoints[2]
 	y1 = roi.getPolygon().ypoints[0]
 	y2 = roi.getPolygon().ypoints[1]
-	angle = roi.getAngle(x1, y1, x2, y2)
-	# TODO: Find a better way to find angle of rotation of the box
-	if angle > 135:
-		angle = angle - 180
-	if angle < -135:
-		angle = 180 - angle
+	y3 = roi.getPolygon().ypoints[2]
+	dist1 = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+	dist2 = math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2)
+	if dist1 > dist2:
+		if y1 > y2:
+			angle = roi.getAngle(x1, y1, x2, y2)
+		else:
+			angle = roi.getAngle(x2, y2, x1, y1)
+	else:
+		if y2 > y3:
+			angle = roi.getAngle(x2, y2, x3, y3)
+		else:
+			angle = roi.getAngle(x3, y3, x2, y2)
+	if angle > 90:
+		angle -= 180
 	# logging.info("\tBounding box x-coord:%s, y-coord:%s, rot-angle:%s" %
 	# 			 (roi.getPolygon().xpoints, roi.getPolygon().ypoints, angle))
 	return angle
@@ -78,42 +88,47 @@ def get_polygon_roi_angle(roi):
 
 def get_rotated_rect_roi_dims(roi):
 	x1,	y1,	x2,	y2,	width = roi.getParams()
-	height = math.floor(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
+	height = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 	# logging.info("Determined rectangular roi height before rounding: %s" % height)
 	# height = round(height / 10) * 10
 	# logging.info("\tRectangular roi height: %s" % height)
-	print("H, W:", (height, width))
-	return (width, height)
+	# print("H, W:", (height, width))
+	return (int(round(width)), int(round(height)))
 
 
 def midpoint(x1, y1, x2, y2):
-    return ((x1 + x2) / 2, (y1 + y2) / 2)
+	x1, y1, x2, y2 = float(x1), float(y1), float(x2), float(y2)
+	return ((x1 + x2) / 2, (y1 + y2) / 2)
 
 def polygon_to_rotated_rect_roi(roi):
 	if isinstance(roi, RotatedRectRoi):
 		return roi
-	x1 = roi.getPolygon().xpoints[0]
-	x2 = roi.getPolygon().xpoints[1]
-	x3 = roi.getPolygon().xpoints[2]
-	x4 = roi.getPolygon().xpoints[3]
-	y1 = roi.getPolygon().ypoints[0]
-	y2 = roi.getPolygon().ypoints[1]
-	y3 = roi.getPolygon().ypoints[2]
-	y4 = roi.getPolygon().ypoints[3]
-	dist1 = math.floor(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
-	dist2 = math.floor(math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2))
+	if roi.getNCoordinates() != 4:
+		raise Exception(
+			"polygon_to_rotated_rect_roi Can only convert rectangles. Received polygon with %s points." % roi.getNCoordinates())
+	x1 = roi.getFloatPolygon().xpoints[0]
+	x2 = roi.getFloatPolygon().xpoints[1]
+	x3 = roi.getFloatPolygon().xpoints[2]
+	x4 = roi.getFloatPolygon().xpoints[3]
+	y1 = roi.getFloatPolygon().ypoints[0]
+	y2 = roi.getFloatPolygon().ypoints[1]
+	y3 = roi.getFloatPolygon().ypoints[2]
+	y4 = roi.getFloatPolygon().ypoints[3]
+	# print("Original polygon: ", x1, y1, x2, y2, x3, y3, x4, y4)
+	dist1 = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+	dist2 = math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2)
 	if dist1 > dist2:
 		width = dist2
 		rx1, ry1 = midpoint(x1, y1, x4, y4)
 		rx2, ry2 = midpoint(x2, y2, x3, y3)
 		rot_rect_roi = RotatedRectRoi(rx1, ry1, rx2, ry2, width)
-		print(x1, y1, x2, y2, width)
+		# print(x1, y1, x2, y2, width)
 	else:
 		width = dist1
 		rx1, ry1 = midpoint(x1, y1, x2, y2)
 		rx2, ry2 = midpoint(x3, y3, x4, y4)
 		rot_rect_roi = RotatedRectRoi(rx1, ry1, rx2, ry2, width)
-		print(x2, y2, x3, y3, width)
+		# print(x2, y2, x3, y3, width)
 	return rot_rect_roi
 
 def check_manual_crop_box():
@@ -121,6 +136,9 @@ def check_manual_crop_box():
 	roi = img.getRoi()
 	roi = polygon_to_rotated_rect_roi(roi)
 	img.setRoi(roi)
+	print("Dims: ", get_rotated_rect_roi_dims(roi))
+	print("Angle: ", get_polygon_roi_angle(roi))
+	# exit()
 	cropped = img.crop()
 	IJ.run(cropped, "Select All", "")
 
