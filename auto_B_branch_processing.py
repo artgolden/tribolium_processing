b66c10693c256127c8b60a345fd808fe973ce252
artgolden <artemiy.golden@protonmail.com>
2022-05-26 23:20:51 +0200

    Added donsampling function

--- auto_B_branch_processing.py
+++ auto_B_branch_processing.py
@@ -1427,5 +1427,40 @@ def split_montage_stack_to_list_of_stacks(montage_image_stack, nrows, ncols, bor
 		list_of_stacks[i] = ImagePlus("New stack from montage %s" % i, new_stack)
 	return list_of_stacks
 
+
+def downsample_image_stack(stack, new_width, sourceSigma=0.5, targetSigma=0.5):
+    """Downsample image stack given its width
+
+    Args:
+        stack (ImagePlus): image stack to downsample
+        new_width (int): new width
+        sourceSigma (float, optional): source image sigma for gaussian blur. Defaults to 0.5.
+        targetSigma (float, optional): target image sigma for gaussian blur. Defaults to 0.5.
+
+    Returns:
+        ImagePlus: downsampled image stack
+    """
+    stack = stack.duplicate()
+    height = int(round(float(new_width) * stack.getHeight() / stack.getWidth()))
+
+    if new_width <= stack.getWidth():
+        s = targetSigma * stack.getWidth() / new_width
+
+        IJ.run(stack, "Gaussian Blur...", "sigma=%s stack" % math.sqrt(s * s - sourceSigma * sourceSigma))
+        IJ.run(stack, "Scale...", "x=- y=- width=%s height=%s process title=- interpolation=None" % (new_width, height))
+
+        extraX = 0 if stack.getWidth() % 2 == 0 else 1
+        extraY = 0 if stack.getHeight() % 2 == 0 else 1
+        initialX = (stack.getWidth() / 2 - new_width / 2 + extraX) if new_width % 2 == 0 else(stack.getWidth() / 2 -
+                                                                                              new_width / 2 + 1 - extraX)
+        initialY = (
+            stack.getHeight() / 2 - height / 2 + extraY) if height % 2 == 0 else (stack.getHeight() / 2 - height / 2 + 1 - extraY)
+        stack.setRoi(initialX, initialY, new_width, height)
+        downsampled_imp = stack.crop("stack")
+    else:
+        IJ.log("You try to upsample the image.  You need an interpolator for that not a downsampler.")
+    return downsampled_imp
+
+
 if __name__ in ['__builtin__', '__main__']:
 	process_datasets(datasets_dir, metadata_file, dataset_name_prefix)
