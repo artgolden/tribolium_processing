550d7c22d36fceee6baffed4007e5912961492cb
artgolden <artemiy.golden@protonmail.com>
2022-06-04 13:39:19 +0200

    Fixed box extraction form xml

--- helper_scripts/get_box_from_xml.py
+++ helper_scripts/get_box_from_xml.py
@@ -6,13 +6,14 @@
 
 import xml.etree.ElementTree as ET
 
-def get_bounding_box_coords_from_file(xml_path, box_name):
+def get_bounding_box_coords_from_xml(xml_path, box_name):
     tree = ET.parse(xml_path)
     root = tree.getroot()
-    for box in root.iter('BoundingBoxDefinition'):
-         if box.attrib["name"] == box_name:
-             min = box.find("min").text.split(" ")
-             max = box.find("max").text.split(" ")
+    for box in root.iter('BoundingBoxes'):
+         if box[0].attrib["name"] == box_name:
+             print(box[0].attrib["name"])
+             min = box[0].find("min").text.split(" ")
+             max = box[0].find("max").text.split(" ")
              box_coords = {
                  "x_min" : int(min[0]),
                  "y_min" : int(min[1]),
@@ -26,5 +27,5 @@ def get_bounding_box_coords_from_file(xml_path, box_name):
 xml_file = xml_file.getAbsolutePath()
 # xml_file = "/run/user/1000/gvfs/smb-share:server=bugcube.physbio.uni-frankfurt.de,share=bugcube/TemporaryData/Artemiy/test_fusion_thresholding/MGolden2022A_DS0039_Sqh5_old_DS0019_manual_crop/dataset.xml"
 
-print get_xml_dict_from_file(xml_path=xml_file, box_name)
+print get_bounding_box_coords_from_xml(xml_path=xml_file, box_name=box_name)
 
