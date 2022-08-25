9c5889f74324f0b746f98f27f15db7ea784260df
artgolden <artemiy.golden@protonmail.com>
2022-08-25 14:05:16 +0200

    Updated rotate files script to handle flipping

--- helper_scripts/rotate_file_and_save_to_folder.py
+++ helper_scripts/rotate_file_and_save_to_folder.py
@@ -1,6 +1,6 @@
 #@ File image_to_rotate
 #@ File(label='Directory to save images', style='directory') output_dir
-#@ String (label='Which direction to rotate?', choices={"Right", "Left"}, style="radioButtonHorizontal", value="Left") rotation_direction
+#@ String (label='How to rotate the image?', choices={"Right", "Left", "Flip Vertically", "Flip Horizontally"}, style="radioButtonHorizontal", value="Left") rotation_direction
 
 image_to_rotate = image_to_rotate
 output_dir = output_dir
@@ -20,7 +20,10 @@ image_path = image_to_rotate.getAbsolutePath()
 output_path = os.path.join(output_dir.getAbsolutePath(), os.path.basename(image_path))
 print("Opening image %s" % image_path)
 imp = IJ.openImage(image_path)
-IJ.run(imp, "Rotate 90 Degrees %s" % rotation_direction, "")
+if rotation_direction in ["Flip Vertically", "Flip Horizontally"]:
+    IJ.run(imp, rotation_direction, "stack")
+else:
+    IJ.run(imp, "Rotate 90 Degrees %s" % rotation_direction, "")
 print("Saving rotated to the %s image %s" % (rotation_direction, output_path))
 save_tiff_simple(imp, output_path)
 
